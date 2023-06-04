78ec666f5b370b297be280769c4febbd1b82cf08
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-06-05 03:21:11 +0500

    Add files via upload

---
+++ boxes.js
@@ -0,0 +1,90 @@
+export default [
+    {
+        id: 1,
+        on: true
+    },   
+    {
+        id: 2,
+        on: false
+    },   
+    {
+        id: 3,
+        on: true
+    },   
+    {
+        id: 4,
+        on: true
+    },   
+    {
+        id: 5,
+        on: false
+    },   
+    {
+        id: 6,
+        on: false
+    },   
+    {
+        id: 7,
+        on: false
+    },  
+    {
+        id: 8,
+        on: false
+    },  
+    {
+        id: 9,
+        on: false
+    },  
+    {
+        id: 10,
+        on: false
+    },  
+    {
+        id: 11,
+        on: false
+    },  
+    {
+        id: 11,
+        on: false
+    },  
+    {
+        id: 12,
+        on: false
+    },  
+    {
+        id: 13,
+        on: false
+    },  
+    {
+        id: 14,
+        on: false
+    },  
+    {
+        id: 15,
+        on: false
+    },  
+    {
+        id: 16,
+        on: false
+    },  
+    {
+        id: 17,
+        on: false
+    },  
+    {
+        id: 18,
+        on: false
+    },  
+    {
+        id: 19,
+        on: false
+    },  
+    {
+        id: 20,
+        on: false
+    },  
+    {
+        id: 21,
+        on: false
+    },  
+]
